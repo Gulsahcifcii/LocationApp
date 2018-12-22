410e614ae6014a364f98c76e693eff6dd60e7532
cengkubilaycicek <kubilaycicek0693@gmail.com>
2018-12-22 16:00:17 +0300

    Tema eklendi

---
+++ LocationApp.Web/Controllers/BlockController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationApp.Web.Controllers
+{
+    public class BlockController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
